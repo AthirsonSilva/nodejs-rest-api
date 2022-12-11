80fcca7489e38934eb57271383090d15835bbbcf
Athirson <athirsonarceus@gmail.com>
2022-12-11 17:46:49 -0300

    feat (api): userController show and index methods

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -3,14 +3,14 @@ import Student from '../models/Student'
 class HomeController {
 	index = async (request, response) => {
 		try {
-			const newStudent = await Student.create(request.body)
+			const newStudent = await Student.findAll()
 
 			response
 				.status(200)
-				.json({ message: 'User created with success!', newStudent })
+				.json({ message: 'Studends fetched with success!', newStudent })
 		} catch (error) {
 			response.status(400).json({
-				message: 'Error at creating new student',
+				message: 'Error at fetch students',
 				error: error.errors.map(e => e.message) ?? error
 			})
 		}
--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -13,6 +13,34 @@ class UserController {
 			})
 		}
 	}
+
+	index = async (request, response) => {
+		try {
+			const users = await User.findAll()
+
+			response.status(200).json({ message: 'Users found with success!', users })
+		} catch (error) {
+			response.status(400).json({
+				message: 'Error at getting users',
+				error: error.errors.map(e => e.message) ?? error
+			})
+		}
+	}
+
+	show = async (request, response) => {
+		try {
+			const user = await User.findByPk(request.params.id)
+
+			if (!user) return response.status(404).json({ message: 'User not found' })
+
+			response.status(200).json({ message: 'User found with success!', user })
+		} catch (error) {
+			response.status(400).json({
+				message: 'Error at getting user',
+				error: error.errors.map(e => e.message) ?? error
+			})
+		}
+	}
 }
 
 export default new UserController()
